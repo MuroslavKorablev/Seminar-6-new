7acb3a2219cb989df8200bd535f7a5df64cf5532
Kleo <mmttdd16@gmail.com>
2023-09-28 06:38:33 +0300

    Finished Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+
+int InputDigit(string message)
+{
+    System.Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!); // Используйте Console.ReadLine() вместо Console.Readline()
+    return number;
+}
+
+string TriangleTrue(int A, int B, int C)
+{
+    if ((A + B) > C && (A + C) > B && (B + C) > A)
+    {
+        return "Может существовать треугольник";
+    }
+    else
+    {
+        return "Не может существовать треугольник";
+    }
+}
+
+int A = InputDigit("Введите длину стороны A: "); // Переменные A, B и C, а не msg
+int B = InputDigit("Введите длину стороны B: "); // Переменные A, B и C, а не msg
+int C = InputDigit("Введите длину стороны C: "); // Переменные A, B и C, а не msg
+
+string result = TriangleTrue(A, B, C);
+Console.WriteLine(result);
