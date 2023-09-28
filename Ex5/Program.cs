5e5dbdd2e6469b9d42b469501f9cdc7dff8783fc
Kleo <mmttdd16@gmail.com>
2023-09-28 13:24:30 +0300

    Finished Ex5

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumberFromUser(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void FibonaciNoRecrution (int count)
+{
+    int firstNumber = 0;
+    int secondNumber = 1;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{firstNumber} ");
+        int sumNumbers = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sumNumbers;
+    }
+}
+
+int number = GetNumberFromUser("Введите число: ");
+FibonaciNoRecrution(number);
