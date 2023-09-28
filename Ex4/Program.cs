0e11fccc6b63e541f29d4ef48e80eb3b96db8608
Kleo <mmttdd16@gmail.com>
2023-09-28 12:50:58 +0300

    Finished Ex4

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumberFromUser(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1; // Базовый случай: факториал 0 равен 1
+    }
+    else
+    {
+        return n * Factorial(n - 1); // Рекурсивный случай
+    }
+}
+
+int number = GetNumberFromUser("Введите число: ");
+int factorialNum = Factorial(number);
+System.Console.WriteLine($"{number} в факториале равно {Factorial(number)}");
