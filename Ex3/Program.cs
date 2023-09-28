03ae3b8215b0a936cecb829789a23569098c7a6c
Kleo <mmttdd16@gmail.com>
2023-09-28 07:08:15 +0300

    Finished Ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int GetNumberFromUser(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+string NumberBitConverter(int num)
+{
+    string binary = Convert.ToString(num, 2);
+    return binary;
+}
+
+int number = GetNumberFromUser("Введите число: ");
+string binaryNumber = NumberBitConverter(number);
+System.Console.WriteLine($"{number} в двоичном коде равно {binaryNumber}");
